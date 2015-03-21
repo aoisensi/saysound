ebbbad7b81ae9ed1ea51f6885cce1fc8bbbfe7c2
aoisensi <aoisensi@mail.com>
2015-03-21 19:01:55 +0900

    yeaaaaaaa

--- saysound.go
+++ saysound.go
@@ -1,43 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"os/user"
-	"path/filepath"
+	"math/rand"
+	"os"
+	"os/exec"
+	"sort"
+	"time"
 )
 
-type config struct {
-	List map[string]string
-}
-
 var (
-	pathRoot, pathConfig, pathSounds string
-	cfg                              config
+	config struct {
+		Sounds map[string]struct {
+			File []string `json:"file"`
+		} `json:"sounds"`
+	}
+	list = flag.Bool("list", false, "show all sound list.")
 )
 
 func init() {
-	u, _ := user.Current()
-	pathRoot = u.HomeDir + "/saysound/"
+	path := os.Getenv("SAYSOUND")
+	if path == "" {
+		path = "$HOME/saysound"
+	}
+	os.Chdir(path)
+
+	c, err := ioutil.ReadFile(path + "/config.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	json.Unmarshal(c, &config)
+	rand.Seed(time.Now().UTC().UnixNano())
 }
 
 func main() {
 	flag.Parse()
-	flag.Arg(0)
-
-	fs, err := ioutil.ReadDir(pathRoot)
-	if err != nil {
-		fmt.Println(err)
+	if *list {
+		l := config.Sounds
+		ls := make([]string, 0, len(l))
+		for key := range l {
+			ls = append(ls, key)
+		}
+		sort.Strings(ls)
+		for _, k := range ls {
+			fmt.Println(k)
+		}
 		return
 	}
-	for _, fi := range fs {
-		if fi.IsDir() {
-			continue
-		}
-		ext := filepath.Ext(fi.Name())
-		if ext != "mp3" && ext != "wav" {
-			continue
-		}
+	args := flag.Args()
+	for _, arg := range args {
+		say(arg)
+		break
 	}
+}
+
+func say(sound string) {
+	snd, ok := config.Sounds[sound]
+	if !ok {
+		return
+	}
+	count := len(snd.File)
+	file := snd.File[rand.Intn(count)]
+	exec.Command("cvlc", "--play-and-exit", file).Start()
 }
